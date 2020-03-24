9ac04cdc4447a49c49dafeb3e66c7a89b9d1e68a
Dario Freddi <dario.freddi@ispirata.com>
2020-03-24 17:53:10 +0100

    Updat copyright notices in client
    Signed-off-by: Dario Freddi <dario.freddi@ispirata.com>

--- client/appengine_devices.go
+++ client/appengine_devices.go
@@ -1,4 +1,4 @@
-// Copyright © 2019 Ispirata Srl
+// Copyright © 2019-2020 Ispirata Srl
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
