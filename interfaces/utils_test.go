327761d8f4b183fb30e55e274cebefdcd8725137
Mattia Mazzucato <mattia.mazzucato@secomind.com>
2023-06-22 10:18:48 +0200

    Update year and company in licenses
    Signed-off-by: Mattia Mazzucato <mattia.mazzucato@secomind.com>

--- interfaces/utils_test.go
+++ interfaces/utils_test.go
@@ -1,4 +1,4 @@
-// Copyright © 2020 Ispirata Srl
+// Copyright © 2020-2023 SECO Mind Srl
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
